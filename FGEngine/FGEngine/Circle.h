87d8bbbdaeed7b5ff50d394e41f4b5794089782e
David Liljefors <david.liljefors@futuregames.nu>
2019-11-11 15:38:29 +0100

    circle takes vec2d as arg

--- FGEngine/FGEngine/Circle.h
+++ FGEngine/FGEngine/Circle.h
@@ -11,6 +11,12 @@ namespace FG
 			position.y = y;
 			radius = r;
 		}
+		Circle(const Vector2D& vec, float r)
+		{
+			position.x = vec.x;
+			position.y = vec.y;
+			radius = r;
+		}
 		Vector2D position = { 0.0f, 0.0f };
 		float radius = 0.0f;
 	};
