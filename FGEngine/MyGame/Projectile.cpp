5ca13ead19855571fa32f048840ef883d25d759a
Rek3dge <samuel.gustafsson@futuregames.nu>
2019-11-12 16:23:04 +0100

    Base Projectile Class Give it a circle colider please

---
+++ FGEngine/MyGame/Projectile.cpp
@@ -0,0 +1,54 @@
+#include "Projectile.h"
+
+#include <Camera.h>
+#include <Sprite.h>
+#include <SDL_render.h>
+#include <Circle.h>
+
+Projectile::Projectile(float speed, float lifetime, FG::Vector2D direction, FG::Camera* camera) :
+	speed(speed), lifetime(lifetime), direction(direction), camera(camera) {}
+
+void Projectile::Update(float deltaTime)
+{
+	isColliding = false;
+	ProjectileUpdate();
+}
+
+void Projectile::Render(FG::Camera* const camera)
+{
+	Entity::Render(camera);
+	sprite->Render(camera, position);
+	DrawBoundingBox();
+}
+
+SDL_Rect Projectile::GetColliderRect()
+{
+	FG::Vector2D finalPosition = position - camera->position;
+	return { static_cast<int>(finalPosition.x), static_cast<int>(finalPosition.y),
+	static_cast<int>(sprite->size.x), static_cast<int>(sprite->size.y) };
+}
+
+void Projectile::OnCollision(FG::Entity* other)
+{
+	//TODO Destroy Projectile
+}
+
+void Projectile::DrawBoundingBox()
+{
+	SDL_Color color = notCollidingColor;
+	if (isColliding)
+	{
+		color = CollidingColor;
+	}
+
+	SDL_Rect finalRect = GetColliderRect();
+	SDL_SetRenderDrawColor(camera->GetInternalRenderer(), color.r, color.g, color.b, color.a);
+	SDL_RenderDrawRect(camera->GetInternalRenderer(), &finalRect);
+	SDL_SetRenderDrawColor(camera->GetInternalRenderer(), 0, 0, 0, 255);
+}
+
+void Projectile::Move(float deltaTime)
+{
+	position += direction * speed * deltaTime;
+}
+
---
+++ FGEngine/MyGame/Projectile.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <Entity.h>
+#include <Vector2D.h>
+
+namespace FG
+{
+	class Window;
+	class Camera;
+	class Sprite;
+}
+
+class Projectile : public FG::Entity
+{
+public:
+	Projectile(float speed, float lifetime, FG::Vector2D direction, FG::Camera* camera);
+
+	void Update(float deltaTime) override;
+	void Render(FG::Camera* const camera) override;
+
+	virtual SDL_Rect GetColliderRect() override;
+	void OnCollision(FG::Entity* other) override;
+
+private:
+	FG::Camera* camera = nullptr;
+
+	bool isColliding = false;
+
+	float speed;
+	float lifetime;
+	FG::Vector2D direction;
+
+	void DrawBoundingBox();
+	void Move(float deltaTime);
+
+	virtual void ProjectileUpdate() = 0;
+	virtual void OnLifetimeEnd() = 0;
+
+	SDL_Color notCollidingColor = { 0, 255, 0, 255 };
+	SDL_Color CollidingColor = { 255, 0, 0, 255 };
+};
+
