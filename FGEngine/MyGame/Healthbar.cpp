85de270e6d750645a40aba64e726e7b25ef9a7b4
David Liljefors <david.liljefors@futuregames.nu>
2019-11-27 11:46:31 +0100

    made healthbar prettier

--- FGEngine/MyGame/Healthbar.cpp
+++ FGEngine/MyGame/Healthbar.cpp
@@ -7,13 +7,23 @@
 void Healthbar::Render(FG::Camera* camera)
 {
 	SDL_Renderer* r = camera->GetInternalRenderer();
-	SDL_Rect rect = { static_cast<int>(position.x), 
-		static_cast<int>(position.y), 
-		static_cast<float>(boss->GetHealth()) / 
-		static_cast<float>(boss->GetMaxHealth()) * 
-		static_cast<int>(size.x), 
-		static_cast<int>(size.y) };
-	SDL_SetRenderDrawColor(r, 255, 0, 0, 0);
+	SDL_Rect rect = 	{
+	static_cast<int>(position.x) - 2,
+	static_cast<int>(position.y) - 2,
+	static_cast<int>(boss->GetHealth() / static_cast<float>(boss->GetMaxHealth())* size.x + 4),
+	static_cast<int>(size.y + 4) 
+	};
+
+	SDL_SetRenderDrawColor(r, 166, 0, 0, 0);
+	SDL_RenderFillRect(r, &rect);
+
+	rect = 	{
+		static_cast<int>(position.x),
+		static_cast<int>(position.y),
+		static_cast<int>(boss->GetHealth() / static_cast<float>(boss->GetMaxHealth())* size.x),
+		static_cast<int>(size.y) 
+	};
+	SDL_SetRenderDrawColor(r, 232, 0, 0, 0);
 	SDL_RenderFillRect(r, &rect);
 	SDL_SetRenderDrawColor(r, 0, 0, 0, 0);
 }
